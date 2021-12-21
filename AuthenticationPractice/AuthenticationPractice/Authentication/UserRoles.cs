43e2853da60e02648e11321f228e017e6eea25de
Ashwani Kumar Vishwakarma <ashwani.vishwakarma@hotmail.com>
2021-12-21 22:02:06 +0530

    JWT Authentication - Using Asp.net Identity

---
+++ AuthenticationPractice/AuthenticationPractice/Authentication/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationPractice.Authentication
+{
+    public class UserRoles
+    {
+        public const string ADMIN = "Admin";
+        public const string USER = "User";
+    }
+}
---
+++ AuthenticationPractice/AuthenticationPractice/Controllers/AuthenticationController.cs
@@ -0,0 +1,144 @@
+﻿using AuthenticationPractice.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            var userExist = await _userManager.FindByNameAsync(registerModel.UserName);
+
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User Already Exist" });
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerModel.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = $"User Creation Failed:{result.Errors.First().Description}" });
+            }
+
+            return Ok(new Response { Status = "Success", Message = "User Created Successfully" });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.UserName);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                foreach (var item in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, item));
+                }
+
+                var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecurityKey"]));
+
+                var token = new JwtSecurityToken
+                    (
+                     issuer: _configuration["JWT:ValidIssuer"],
+                     audience: _configuration["JWT:ValidAudience"],
+                     claims: authClaims,
+                     expires: DateTime.Now.AddHours(5),
+                     signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new 
+                { 
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = DateTime.Now.AddHours(5)
+                });
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost]
+        [Route("RegisterAdmin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel registerModel)
+        {
+            var userExist = await _userManager.FindByNameAsync(registerModel.UserName);
+
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User Already Exist" });
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerModel.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = $"User Creation Failed:{result.Errors.First().Description}" });
+            }
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.ADMIN))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.ADMIN));
+            }
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.USER))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.USER));
+            }
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.ADMIN))
+            {
+                await _userManager.AddToRoleAsync(user, UserRoles.ADMIN);
+            }
+
+            return Ok(new Response { Status = "Success", Message = "User Created Successfully" });
+        }
+    }
+}
